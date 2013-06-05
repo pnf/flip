d772c942fb7cb319e8ffe258f855f88534932ebb
Peter Fraenkel <pnf@podsnap.com>
2013-06-05 18:34:24 -0400

    Sort of works

--- src/main/scala/Flippotron.scala
+++ src/main/scala/Flippotron.scala
@@ -1,54 +1,103 @@
 package com.podsnap.flip
 import com.typesafe.scalalogging.slf4j.Logging
-
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
 //import java.awt.FlowLayout
 import scala.swing._
 //import javax.swing._
 
+// The best way to make something happen on the UI thread is to publish an event, which requires
+// being in a class that extends Publisher.
 private case class StatusEvent(i : Int) extends event.Event {}
+private case class StoppedFlippingEvent() extends event.Event {}
 
-object Flippotron  extends SimpleSwingApplication with Publisher with Logging {
-  val file = "bleh.pdf"
-  val positionField = new TextField("1",5) {editable = false}
-  val statusField = new TextField("",5) {editable = false}
-  val flip = new Label()
-  val smoother = new Smoother("/home/pnf/Downloads/scala-swing-design.pdf", { i : Int => publish(new StatusEvent(i))})
-  val numPages = smoother.numPages
-  val pageSlider = new Slider {
-    min = 1
-    max = numPages
-    preferredSize = new Dimension(500,20)
-  }
-  val controlPanel = new BoxPanel(Orientation.Horizontal) {
-    contents += positionField
-    contents += pageSlider
-    contents += statusField
-  }
-  val flipPanel = new FlowPanel() {
-    contents += flip
+object Flippotron  extends Logging {
+
+  var file : String = _
+  def main(args : Array[String]) {
+    file = if(args.length>0) args(0) else "/home/pnf/Downloads/scala-swing-design"
+    GUI.main(args)
   }
 
-  def top = new MainFrame {
-    title = "Flippotron"
-    contents = new BoxPanel(Orientation.Vertical) {
-      contents += controlPanel
-      contents += flipPanel
+
+  object GUI extends SimpleSwingApplication with Publisher with Logging {
+
+    val positionField = new TextField("1",5) {editable = false}
+    val statusField = new TextField("",5) {editable = false}
+    val flip = new Label()
+    val smoother = new Smoother(file, { i : Int => publish(new StatusEvent(i))})
+    val numPages = smoother.numPages
+    val pageSlider = new Slider {
+      min = 1
+      max = numPages
+      preferredSize = new Dimension(500,20)
     }
-    listenTo(pageSlider, Flippotron)
-    reactions += {
-      case swing.event.ValueChanged(`pageSlider`) =>
-        val i = pageSlider.value
-        positionField.text = i+""
-        flip.icon = smoother.origPage(i)
-        pack
-      case StatusEvent(i) =>
-        logger.debug(s"received status event $i")
-        statusField.text = numPages + "(" + i + ")"
-        if(i<=1) {
-          flip.icon = smoother.origPage(i)
-          pack()
+    val controlPanel = new BoxPanel(Orientation.Horizontal) {
+      contents += positionField
+      contents += pageSlider
+      contents += statusField
+    }
+    val flipPanel = new FlowPanel() {
+      contents += flip
+    }
+
+    val rest = new java.util.concurrent.atomic.AtomicLong()
+    val timer : Future[Unit] = future {
+      // send a stopped flipping event in 10 seconds
+      rest.set(now)   // time of last motion
+      while (true){
+        val dt = scala.compat.Platform.currentTime - rest.get()
+        if(dt>=1000) {                 scala.compat.Platform.currentTime
+          publish(new StoppedFlippingEvent())
+          Thread.sleep(1000)
+        } else {
+          Thread.sleep(1000-dt)
         }
+      }
+    }
+
+    def now : Long = {
+      scala.compat.Platform.currentTime
+    }
+
+    def top = new MainFrame {
+      title = "Flippotron"
+      contents = new BoxPanel(Orientation.Vertical) {
+        contents += controlPanel
+        contents += flipPanel
+      }
+      listenTo(pageSlider, GUI)
+      var lastp = 1
+      var lastnow = now
+      reactions += {
+        case swing.event.ValueChanged(`pageSlider`) =>
+          val i = pageSlider.value
+          val newnow = now
+          val dt = newnow - lastnow
+          logger.debug(s"received slider event $i $dt")
+          positionField.text = i+""
+          if(dt > 100) {
+            flip.icon = smoother.origPage(i)
+          } else {
+            flip.icon = smoother.blurredPage(0,i)
+            rest.set(now)
+          }
+          lastnow = newnow
+          lastp = i
+          pack()
+        case StatusEvent(i) =>
+          logger.debug(s"received status event $i")
+          statusField.text = numPages + "(" + i + ")"
+          if(i<=1) {
+            flip.icon = smoother.origPage(i)
+            pack()
+          }
+        case StoppedFlippingEvent() =>
+          logger.debug(s"stopped flipping event")
+          flip.icon = smoother.origPage(lastp)
+
+      }
     }
   }
 }
--- src/main/scala/Smoother.scala
+++ src/main/scala/Smoother.scala
@@ -2,12 +2,12 @@ package com.podsnap.flip
 import java.io._
 import java.nio.channels.FileChannel
 import javax.swing.ImageIcon
-import java.awt.Rectangle
-import scala.concurrent.future
-import scala.concurrent.ExecutionContext.Implicits.global
-import com.sun.pdfview.PDFFile  // Why does it claim this is an error when sbt doesn't think it is?
-
-// TODO: figure out how to run something in the background and listen for it being done
+import java.awt.{Graphics, Rectangle}
+import com.sun.pdfview.PDFFile
+import java.awt.image.BufferedImage
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
 class Smoother(pdf : String, progress : Int => Unit = null) {
 
@@ -21,11 +21,22 @@ class Smoother(pdf : String, progress : Int => Unit = null) {
   tmpdir.delete()
   tmpdir.mkdir()
 
-  val page = pdffile.getPage(0)
-  val w : Int = page.getBBox().getWidth().toInt
-  val h : Int = page.getBBox().getHeight().toInt
-  val rect = new Rectangle(0,0,w,h)
+  val navg=8
+  val bookend = 8
 
+  val page = pdffile.getPage(0)
+  val wr : Int = page.getBBox().getWidth().toInt
+  val hr : Int = page.getBBox().getHeight().toInt
+  val rect = new Rectangle(0,0,wr,hr)
+  val w = wr
+  val h = hr
+
+
+  val files = new Array[File](numPages)
+
+  val done = new AtomicInteger(0)
+
+  // Possibly an abuse of future to launch an unsupervised thread
   future {
     write()
   }
@@ -44,17 +55,151 @@ class Smoother(pdf : String, progress : Int => Unit = null) {
     ic
   }
 
+  def blurredPage(res : Int, p : Int) = {
+    if(p<=bookend || p >=(numPages-bookend) || p>done.get())
+      origPage(p);
+    else
+      new ImageIcon(imageFile(0,p).getAbsolutePath())
+  }
+
+  private def imageFile(res : Int, p:Int) : File = {
+    if(files(p)==null)
+    files(p) = new File(tmpdir.getAbsolutePath(),"page_" + res + "_" + p + ".png");
+    files(p)
+  }
+
   def write() {
-    val numPgs = pdffile.getNumPages();
-    for(i <- 1 to numPgs) {
-      Thread.sleep(500)
-      if(progress!=null) progress(i)  // this feels wrong
+    val numPgs = pdffile.getNumPages()
+    val pixelsi = new Array[Int](h*w)
+    val rollingSum = Array.fill[Long](h*w)(0)
+    val hist = Array.ofDim[Long](numPgs,h*w)
+    var bimage = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR)
+
+      //		BufferedImage simage = null;
+      //	    float data[] = { 0.0625f, 0.125f, 0.0625f, 0.125f, 0.25f, 0.125f,
+      //	            0.0625f, 0.125f, 0.0625f };
+      //	    Kernel kernel = new Kernel(3, 3, data);
+      //	    ConvolveOp convolve = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
+
+    for(i<-0 until numPgs) {
+
+        //generate page image
+        var image = pdffile.getPage(i).getImage(w,h,rect,null,true,true)
+        // force complete loading
+        image = new ImageIcon(image).getImage()
+        // Copy image to buffered image
+        val g = bimage.createGraphics()
+        // Paint the image onto the buffered image
+        g.drawImage(image, 0, 0, null)
+        g.dispose()
+
+        // extract pixels into array
+        bimage.getRGB(0, 0, w, h, pixelsi, 0, w)
+        // Accumulate rolling averages
+        val im = i%navg
+        val ii = i-navg/2; // middle of averaging range
+        for(j <- 0 until h*w) {
+          var p = pixelsi(j)
+          var q=0L
+
+          // Expand packed 8x3 pixel to 16x3
+          var r = 0L
+          var r2 = 0L
+          r |= (p&0xff); r<<=16; p>>=8;
+          r |= (p&0xff); r<<=16; p>>=8;
+          r |= (p&0xff);
+          // complement, so black gets averaged to black
+          r = ~r
+          rollingSum(j) = rollingSum(j) + r
+
+          if(i>=navg) { // we have enough to average
+            rollingSum(j) = rollingSum(j) - hist(im)(j);    // roll off the old
+            hist(im)(j) = r
+
+            rollingSum(j)*3
+
+            r2 = (rollingSum(j)*3L/navg + r)/4;
+            r = ~r2;
+          } else {
+            hist(im)(j) = r;
+          }
+
+          // Repack averaged pixel
+          q |= (r&0xff); q<<=8; r>>=16;
+          q |= (r&0xff); q<<=8; r>>=16;
+          q |= (r&0xff);
+
+        //	Average over number of images frames with a non-background pixel in this location.
+        //  Note that we sum in complement space, so background is zero.
+        //				if(i>=navg)
+        //				nfg[j] -= fghist[im][j];
+        //			nfg[j] += (fghist[im][j] = (q==-1) ? 0 : 1);
+        //
+        //				// If all pixels in history were background, this is easy...
+        //				if(nfg[j]==0)
+        //					q = -1;
+        //
+        //				else {
+        //
+        //					if(i>=navg) sum[k]-=hist[im][k];
+        //					hist[im][k] = ~(p&0xff);
+        //					sum[k] += hist[im][k];
+        //					if(i>=navg)
+        //						q += ~(((sum[k]/nfg[j])*3 + hist[iim][k])>>2);
+        //					else if(i>=navg/2)
+        //						q += ~(sum[k]/nfg[j] + hist[i-navg/2][k])>>1;
+        //					else
+        //						q += ~(sum[k]/nfg[j]);
+        //					k++; q<<=8; p>>=8;
+        //
+        //					if(i>=navg) sum[k]-=hist[im][k];
+        //					hist[im][k] = ~(p&0xff);
+        //					sum[k] += hist[im][k];
+        //					if(i>=navg)
+        //						q += ~(((sum[k]/nfg[j])*3 + hist[iim][k])>>2);
+        //				else if(i>=navg/2)
+        //				q += ~((sum[k]/nfg[j] + hist[i-navg/2][k])>>1);
+        //			else
+        //				q += ~(sum[k]/nfg[j]);
+        //			k++; q<<=8; p>>=8;
+        //
+        //					if(i>=navg) sum[k]-=hist[im][k];
+        //					hist[im][k] = ~(p&0xff);
+        //					sum[k] += hist[im][k];
+        //					if(i>=navg)
+        //						q += ~(((sum[k]/nfg[j])*3 + hist[iim][k])>>2);
+        //					else if(i>=navg/2)
+        //						q += ~((sum[k]/nfg[j] + hist[i-navg/2][k])>>1);
+        //					else
+        //						q += ~(sum[k]/nfg[j]);
+        //					k++;
+        //				}
+
+        pixelsi(j) = q.toInt;
+      }
+      bimage = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+      bimage.setRGB(0, 0, w, h, pixelsi, 0, w);
+
+      //save it as a file
+      if(i>=navg) {
+        try {
+          javax.imageio.ImageIO.write( bimage,"png",imageFile(0,ii+1))
+        }
+        catch {
+          case e : Throwable =>
+            throw new RuntimeException(e.getMessage());
+        }
+      }
+      if(progress!=null) progress(ii+1)
+
+      done.set(ii)
+
+      // todo: handle termination
+
     }
   }
 
 }
-
-
 
 
 
